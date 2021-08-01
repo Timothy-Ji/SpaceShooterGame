0e17a32edec1ff0c76921bbf282e4b6d24c593af
Timothy Ji <JiTimothy@outlook.com>
2021-07-31 23:00:05 -0400

    changed file structure and added github page to pause menu

--- pause/PauseMenu.gd
+++ pause/PauseMenu.gd
@@ -4,3 +4,7 @@ signal volume_slider_value_changed(value)
 
 func _on_VolumeSlider_value_changed(value):
 	emit_signal("volume_slider_value_changed", value)
+
+
+func _on_GithubLinkButton_pressed():
+	OS.shell_open("https://github.com/Timothy-Ji/SpaceShooterGame")
